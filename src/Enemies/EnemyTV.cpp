293ce47263cc00f0b20c22f8499aea716e96080c
Vlad <titkov2002@gmail.com>
2025-10-04 22:19:14 +0600

    improve tv moving

--- src/Enemies/EnemyTV.cpp
+++ src/Enemies/EnemyTV.cpp
@@ -3,11 +3,20 @@
 #include <iostream>
 #include <VVADExtras.h>
 
+bool IsCameraLookingAtObject(Camera3D camera, Vector3 objectPosition, float threshold = 0.8f)
+{
+    Vector3 cameraForward = Vector3Normalize(Vector3Subtract(camera.target, camera.position));
+    Vector3 toObject = Vector3Normalize(Vector3Subtract(objectPosition, camera.position));
+    float dot = Vector3DotProduct(cameraForward, toObject);
+
+    return (dot >= threshold);
+}
+
 EnemyTV::EnemyTV()
 {
     modelTV = LoadModel("resources/TV.obj");
-    modelTV.transform = TransformToMatrix({{0.f, 0.f, 0.f}, QuaternionFromEuler(0, 0, 0), {1, 1, 1}});
-    objectTransform = {{0.f, 0.f, 0.f}, QuaternionFromEuler(0, 0, 0), {1, 1, 1}};
+    objectTransform = {{0.f, 0.f, 3.f}, QuaternionFromEuler(0, 0, 0), {1, 1, 1}};
+    modelTV.transform = TransformToMatrix(objectTransform);
 }
 
 EnemyTV::~EnemyTV()
@@ -18,17 +27,27 @@ EnemyTV::~EnemyTV()
 void EnemyTV::Update(float dt)
 {
     PlayerFP &playerInstance = PlayerFP::Instance();
+    CollisionManager& cMngrInsance = CollisionManager::Instance();
 
-    Vector3 dir = Vector3Subtract(playerInstance.position, objectTransform.translation);
+    Vector3 dir = Vector3Subtract(playerInstance.camera.position, objectTransform.translation);
     Vector3 dirNorm = Vector3Normalize(dir);
 
-    Vector3 fwd = Vector3Subtract(playerInstance.position, objectTransform.translation);
-    Quaternion qForward = QuaternionFromForward(fwd);  
+    Quaternion qForward = QuaternionFromForward(dir);  
     Quaternion qFix = QuaternionFromAxisAngle( { 0, 1, 0 }, PI/2);
     Quaternion q = QuaternionSlerp(objectTransform.rotation, QuaternionMultiply(qForward, qFix), 0.05);
+    
+    Vector3 direction = playerInstance.camera.position - objectTransform.translation;
+    float distance = Vector3Length(direction);
+
+    Ray r = { objectTransform.translation, Vector3Normalize(direction) };
+    RayCollision collision = cMngrInsance.GetRayCollision(r,true);
+    
+    bool isLooking = IsCameraLookingAtObject(playerInstance.camera, objectTransform.translation);
+
+    std::cout<< IsCameraLookingAtObject(playerInstance.camera, objectTransform.translation) << std::endl;
 
     SetTranform(
-        {objectTransform.translation + dirNorm * 0.05,
+        {objectTransform.translation + dirNorm * !isLooking * 0.05,
          q,
          objectTransform.scale});
 }
